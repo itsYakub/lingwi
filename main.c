1d07f2f29525cb966dbc2355b67d5e938813a929
Jakub Oleksiak <yakubofficialmail@gmail.com>
2024-11-13 22:11:58 +0100

    Huge improvements
    Translator now has a better parser that uses a built-in sscanf libc function. What's more, languages are now validated based on the ISO-639 set 1. language code list. Parser is now bound to the engine implementation. Lingwi now has a way to add more engines, especially when they support the lang-code standard

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 // + -------------------------------------- +
 // | Translation command-line utility.      |
 // + -------------------------------------- +
+// | Licence: bottom of the file            |
+// + -------------------------------------- +
 
 // SECTION: Inclusions
 #include <stdio.h>
@@ -19,43 +21,77 @@
 
 // SECTION: Macro definitions
 #define LINGWI_VERSION "1.0"
-#define LINGWI_IO_SIZE 5000
+#define LINGWI_IO_SIZE 5001
 #define LINGWI_URL_SIZE (LINGWI_IO_SIZE + 128)
+#define LINGWI_ISO639_1_LANGCODE_COUNT 184
 
 // SECTION: Function declarations
 size_t callback(char* ptr, size_t size, size_t nmemb, char* data);
-int process_options(int ac, char** av, char* sl, char* tl, char* engine, char* text);
 
-int translate(char* engine, char* dest, char* src, char* sl, char* tl);
+int process_options(int ac, char** av, char* sl, char* tl, char* engine, char* apikey, char* text);
+
+int translate(char* engine, char* apikey, char* dest, char* src, char* sl, char* tl);
 int translate_engine_google(char* dest, char* src, char* sl, char* tl);
 
-int parse(char* str);
+int lang_valid(char* lang);
 
 // SECTION: Global variables
 static struct option long_option_s[] = {
     { "source-language", required_argument, NULL, 's' },
     { "target-language", required_argument, NULL, 't' },
     { "engine", required_argument, NULL, 'e' },
+    { "api-key", required_argument, NULL, 'a' },
     { "version", no_argument, NULL, 'v' },
     { "help", no_argument, NULL, 'h' },
     { 0, 0, 0, 0 }
 };
 
-int main(int ac, char** av) {
-    char input[LINGWI_IO_SIZE]; 
-    char output[LINGWI_IO_SIZE];
+// Source:
+// - https://en.wikipedia.org/wiki/ISO_639
+// - https://www.iso.org/iso-639-language-code
+// Array generated via ChatGPT
+static const char* ISO_639_1_LANGUAGE_CODES[LINGWI_ISO639_1_LANGCODE_COUNT] = {
+    "aa", "ab", "ae", "af", "ak", "am", "an", "ar", "as", "av",
+    "ay", "az", "ba", "be", "bg", "bh", "bi", "bm", "bn", "bo",
+    "br", "bs", "ca", "ce", "ch", "co", "cr", "cs", "cu", "cv",
+    "cy", "da", "de", "dv", "dz", "ee", "el", "en", "eo", "es",
+    "et", "eu", "fa", "ff", "fi", "fj", "fo", "fr", "fy", "ga",
+    "gd", "gl", "gn", "gu", "gv", "ha", "he", "hi", "ho", "hr",
+    "ht", "hu", "hy", "hz", "ia", "id", "ie", "ig", "ii", "ik",
+    "io", "is", "it", "iu", "ja", "jv", "ka", "kg", "ki", "kj",
+    "kk", "kl", "km", "kn", "ko", "kr", "ks", "ku", "kv", "kw",
+    "ky", "la", "lb", "lg", "li", "ln", "lo", "lt", "lu", "lv",
+    "mg", "mh", "mi", "mk", "ml", "mn", "mr", "ms", "mt", "my",
+    "na", "nb", "nd", "ne", "ng", "nl", "nn", "no", "nr", "nv",
+    "ny", "oc", "oj", "om", "or", "os", "pa", "pi", "pl", "ps",
+    "pt", "qu", "rm", "rn", "ro", "ru", "rw", "sa", "sc", "sd",
+    "se", "sg", "si", "sk", "sl", "sm", "sn", "so", "sq", "sr",
+    "ss", "st", "su", "sv", "sw", "ta", "te", "tg", "th", "ti",
+    "tk", "tl", "tn", "to", "tr", "ts", "tt", "tw", "ty", "ug",
+    "uk", "ur", "uz", "ve", "vi", "vo", "wa", "wo", "xh", "yi",
+    "yo", "za", "zh", "zu"
+};
 
-    char sl[8] = "auto";
+int main(int ac, char** av) {
+    char input[LINGWI_IO_SIZE] = { 0 }; 
+    char output[LINGWI_IO_SIZE] = { 0 };
+
+    char sl[8] = "en";
     char tl[8] = "en";
     char engine[8] = "google";
+    char apikey[128];
 
-    process_options(ac, av, sl, tl, engine, input);
-    translate(engine, output, input, sl, tl);
-    parse(output);
+    if(ac <= 1) {
+        fprintf(stderr, "%s -[OPTIONS] [TEXT] ... \n", av[0]);
+        return EXIT_FAILURE;
+    }
+
+    process_options(ac, av, sl, tl, engine, apikey, input);
+    translate(engine, apikey, output, input, sl, tl);
 
     fprintf(stdout, "%s\n", output);
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 size_t callback(char* ptr, size_t size, size_t nmemb, char* data) {
@@ -66,10 +102,10 @@ size_t callback(char* ptr, size_t size, size_t nmemb, char* data) {
     return bytes;
 }
 
-int process_options(int ac, char** av, char* sl, char* tl, char* engine, char* text) {
+int process_options(int ac, char** av, char* sl, char* tl, char* engine, char* apikey, char* text) {
     // Processing program options
     char opt;
-    while((opt = getopt_long(ac, av, "stvh", long_option_s, NULL)) != -1) {
+    while((opt = getopt_long(ac, av, "stavh", long_option_s, NULL)) != -1) {
         switch(opt) {
             case 's': {
                 strcpy(sl, optarg);
@@ -83,13 +119,21 @@ int process_options(int ac, char** av, char* sl, char* tl, char* engine, char* t
                 strcpy(engine, optarg);
             } break;
 
+            case 'a': {
+                strcpy(apikey, optarg);
+            } break;
+
             case 'h':
                 fprintf(stdout, "\033[1mlingwi\033[0m - translation command-line utility\n");
-                fprintf(stdout, "  \033[1m--s\033[0m, \033[1m--source-language\033[0m - select a source language (translation from...) (DEFAULT: auto)\n");
+                fprintf(stdout, "  \033[1m--s\033[0m, \033[1m--source-language\033[0m - select a source language (translation from...) (DEFAULT: en)\n");
                 fprintf(stdout, "  \033[1m--t\033[0m, \033[1m--target-language\033[0m - select a target language (translation to...) (DEFAULT: en)\n");
                 fprintf(stdout, "  \033[1m--e\033[0m, \033[1m--engine\033[0m - select a translation engine (DEFAULT: google)\n");
+                fprintf(stdout, "  \033[1m--a\033[0m, \033[1m--api-key\033[0m - select an api key (DEFAULT: none)\n");
                 fprintf(stdout, "  \033[1m--v\033[0m, \033[1m--version\033[0m - display a version (%s)\n", LINGWI_VERSION);
-                fprintf(stdout, "  \033[1m--h\033[0m, \033[1m--help\033[0m - display a help message\n");
+                fprintf(stdout, "  \033[1m--h\033[0m, \033[1m--help\033[0m - display a help message\n\n");
+                fprintf(stdout, "\033[1mExample language codes:\033[0m\n   | en | es | ja | de | ru | pl | bo |\n"); 
+                fprintf(stdout, "\033[1mAll language codes:\033[0m\n   https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes\n"); 
+                
                 exit(EXIT_SUCCESS);
 
             case 'v':
@@ -111,8 +155,12 @@ int process_options(int ac, char** av, char* sl, char* tl, char* engine, char* t
     return 1;
 }
 
-int translate(char* engine, char* dest, char* src, char* sl, char* tl) {
+int translate(char* engine, char* apikey, char* dest, char* src, char* sl, char* tl) {
+    assert(lang_valid(sl) == 1);
+    assert(lang_valid(tl) == 1);
+
     if(strcmp(engine, "google") == 0) {
+        (void) apikey;
         translate_engine_google(dest, src, sl, tl);
     } else {
         fprintf(stderr, "[ ERR ] Invalid engine\n");
@@ -122,6 +170,7 @@ int translate(char* engine, char* dest, char* src, char* sl, char* tl) {
 }
 
 int translate_engine_google(char* dest, char* src, char* sl, char* tl) {
+    // Setup
     CURL* curl;
     CURLcode ccode;
 
@@ -146,29 +195,23 @@ int translate_engine_google(char* dest, char* src, char* sl, char* tl) {
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, dest);
 
+    // Translation
     ccode = curl_easy_perform(curl);
     assert(ccode == CURLE_OK);
     curl_easy_cleanup(curl);
-    
+   
+    // Parsing
+    // TODO: Modifing the source language leaves the '[' and '"' signs
+    sscanf(dest, "[\"%5000[^\"]", dest);
+
     return 1;
 }
 
-int parse(char* str) {
-    // Parsing works pretty simple:
-    // 1. Get the start of the translated string (the first appearing " char)
-    // 2. Get the end of the translated string (the closing " char)
-    // 3. Copy the text in between two pointers to the str string via strncpy)
+int lang_valid(char* lang) {
+    for(int i = 0; i < LINGWI_ISO639_1_LANGCODE_COUNT; i++) {
+        if(strcmp(lang, ISO_639_1_LANGUAGE_CODES[i]) == 0)
+            return 1;
+    }    
 
-    char* strs = str;
-    char* stre = NULL;
-
-    for(; strs && *strs != '\"'; strs++) ;
-    stre = ++strs;
-    for(; stre && *stre != '\"'; stre++) ;
-
-    int nslen = stre - strs;
-    strncpy(str, strs, nslen);
-    str[nslen] = 0;
-
-    return 1;
+    return 0;
 }
